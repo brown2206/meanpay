6e8112927f895d6d3eddb4e3e63aab25d71730b6
brown2206 <brown.angelo.jr@gmail.com>
2016-11-28 13:57:39 -0500

    change date

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 var express = require("express")
 var hbs = require("express-handlebars")
-var path = require("path")  //may not need this
-var app = express()
-var bodyParser = require("body-parser")
-var cookieParser = require("cookie-parser")
 var mongoose = require("mongoose")
-var bcrypt = require("bcryptjs")
-var expressValidator = require("express-validator")
-var flash = require("connect-flash")
-var session = require("express-session")
-var passport = require("passport")
-var LocalStrategy = require("passport-local").Strategy
+var Payee = mongoose.model("Payee")
+var bodyParser = require("body-parser")
+
+var app = express()
+
+// var path = require("path")  //may not need this
+// var cookieParser = require("cookie-parser")
+// var bcrypt = require("bcryptjs")
+// var expressValidator = require("express-validator")
+// var flash = require("connect-flash")
+// var session = require("express-session")
+// var passport = require("passport")
+// var LocalStrategy = require("passport-local").Strategy
 
 // var routes = require("./routes/index")
 // var users = require("./routes/users")
 // var payees = require("./routes/payees")
 
 app.set("view engine","hbs")
+
 app.get("/", (req, res) => {
   res.send("Hello World & Lil' Mama XYZ123 + Hangtime!!!")
 })
 
-app.use(cookieParser())
+// app.use(cookieParser())
 
 app.use(bodyParser.urlencoded({extended: true})) //handle forms
 
