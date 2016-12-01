0b2ac641c01d04a1c9547ab64d97f1d51eea8820
brown2206 <brown.angelo.jr@gmail.com>
2016-12-01 05:41:57 -0500

    full crud backend

--- app.js
+++ app.js
@@ -1,25 +1,13 @@
-var express = require("express")
-var hbs = require("express-handlebars")
-var mongoose = require("./db/schema")
-var Payee = mongoose.model("Payee")
-var methodOverride = require("method-override")
-var _ = require("lodash")
-var bodyParser = require("body-parser")
+var express = require('express')
+var bodyParser = require('body-parser')
+var hbs = require('express-handlebars')
+var mongoose = require('./db/schema')
+var Payee = mongoose.model('Payee')
+var methodOverride = require('method-override')
+var _ = require('lodash')
 
 var app = express()
 
-// var path = require("path")  //may not need this
-// var cookieParser = require("cookie-parser")
-// var bcrypt = require("bcryptjs")
-// var expressValidator = require("express-validator")
-// var flash = require("connect-flash")
-// var session = require("express-session")
-// var passport = require("passport")
-// var LocalStrategy = require("passport-local").Strategy
-
-// var routes = require("./routes/index")
-// var users = require("./routes/users")
-// var payees = require("./routes/payees")
 
 // Load the models
 app.models = require('./db/schema.js')
@@ -45,7 +33,7 @@ app.use(methodOverride('X-HTTP-Method-Override'))
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET','PUT','POST','DELETE')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
   res.header('Access-Control-Allow-Headers', 'Content-Type')
   next()
 })
